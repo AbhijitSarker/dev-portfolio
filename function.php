2f0e2b3779dcd62d8ea135ee44b78e84f1344e66
Abhijitsarker <abhijitsarker03@gmail.com>
2023-01-27 03:29:53 +0600

    first commit

---
+++ function.php
@@ -0,0 +1,29 @@
+<?php
+
+//defining templates directory paath
+define('TEMPLATES_DIRECTORY_PATH', get_template_directory_uri());
+
+
+add_action('wp_enqueue_scripts', 'devfolio_theme_css_js');
+
+function devfolio_theme_css_js()
+{
+    // CSS Files
+    wp_enqueue_style('font-awesome', TEMPLATES_DIRECTORY_PATH . '/assets/css/fontawesome.min.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('magnific-popup', TEMPLATES_DIRECTORY_PATH . '/assets/css/magnific-popup.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('owl-carousel', TEMPLATES_DIRECTORY_PATH . '/assets/css/owl.carousel.min.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('bootstrap', TEMPLATES_DIRECTORY_PATH . '/assets/css/bootstrap.min.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('style', TEMPLATES_DIRECTORY_PATH . '/assets/css/style.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('responsive-style', TEMPLATES_DIRECTORY_PATH . '/assets/css/responsive.css', array(), '1.0.0', 'all');
+
+    // JS Files
+    wp_enqueue_script('easy-piechart', TEMPLATES_DIRECTORY_PATH . '/assets/js/jquery.easypiechart.min', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('magnific-popup', TEMPLATES_DIRECTORY_PATH . '/assets/js/jquery.magnific-popup.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('owl-carousel', TEMPLATES_DIRECTORY_PATH . '/assets/js/owl.carousel.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('bootstrap', TEMPLATES_DIRECTORY_PATH . '/assets/js/bootstrap.bundle.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('gsap', TEMPLATES_DIRECTORY_PATH . '/assets/js/gsap.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('ScrollToPlugin', TEMPLATES_DIRECTORY_PATH . '/assets/js/ScrollToPlugin.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('ScrollSmoother', TEMPLATES_DIRECTORY_PATH . '/assets/js/ScrollSmoother.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('splittext', TEMPLATES_DIRECTORY_PATH . '/assets/js/splittext.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('app', TEMPLATES_DIRECTORY_PATH . '/assets/js/app.js', array('jquery'), '1.0.0', true);
+}
---
+++ header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Abhijit Sarker</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.png" />
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+
+<body>
+    <!-- Cursor Animation -->
+    <div class="cursor1"></div>
+    <div class="cursor2"></div>
+    <!-- =============Start-header============== -->
+    <div class="wrapper">
+        <header>
+            <nav class="navigation">
+                <div class="container">
+                    <div class="header-wrapper">
+                        <div class="row align-items-center">
+                            <div class="col-xl-3 col-lg-2">
+                                <div class="row align-items-center">
+                                    <div class="col-lg-12 col-6">
+                                        <a href="index.html" class="logo">
+                                            <img src="img/logo.png" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="d-lg-none d-flex justify-content-end col-6">
+                                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                            <i class="fa-solid fa-bars"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-9 col-lg-10">
+                                <nav class="navbar navbar-expand-lg p-0">
+                                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                        <ul class="navbar-nav ms-auto align-items-lg-center">
+                                            <li class="nav-item dropdown">
+                                                <a class="nav-link dropdown-toggle" href="index.php" id="HomeDropdown" role="button">
+                                                    Home
+                                                </a>
+
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#About">About</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#Service">Service</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#Portfolio">Portfolio</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#Contact">Contact</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#Blog">Blog</a>
+                                            </li>
+                                            <li class="nav-btn">
+                                                <a href="#" class="def-btn btn-hover"><span class="dot"></span> <span class="txt">Get Started</span></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    </div>
+    <!-- ===================End-Header==================== -->
